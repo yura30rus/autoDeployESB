767b68432ac190bea669c91a5e44d7c04d7c063f
yumochalkin <yumochalkin@digital-spirit.ru>
2022-12-01 01:17:21 +0400

    Some fix for override

--- main.go
+++ main.go
@@ -19,41 +19,42 @@ func main() {
 	allApp := flag.Bool("all", false, "deploy all service")
 	changeApp := flag.String("n", "", "deploy selected app by name")
 	flag.Parse()
-
+	s := pkg.ReadFile(linuxPath)
 	switch {
 	case *allApp:
-		fmt.Println("test")
-	case *changeApp == "":
-		fmt.Println("Your app is")
+		runAllCommand(s)
+	case *changeApp != "":
+		runOneCommand(s, *changeApp)
 	}
+}
 
-	fmt.Println("Hello, DigitalOcean!")
+func runOneCommand(s []pkg.App, goal string) {
+	fmt.Printf("start deploy %s service\n", goal)
+	for i := range s {
+		if s[i].AppName == goal {
+			runCommand(s[i].Command, s[i].AppName)
+			break
+		}
 
-	s := pkg.ReadFile(linuxPath)
-	fmt.Println(s[1].AppName)
-	fmt.Println(s[1].Command)
-
-	runCommand(s[1].Command, s[1].AppName)
-
+	}
+	fmt.Printf("%s override", goal)
 }
 
 func runAllCommand(s []pkg.App) {
-	for range s {
-
+	fmt.Println("start deploy all service")
+	for i := range s {
+		runCommand(s[i].Command, s[i].AppName)
 	}
+	fmt.Println("All override")
 }
 
 func runCommand(command []string, appName string) {
-
 	barName := fmt.Sprintf(appName + ".bar")
-	fmt.Println(command[0])
 	for _, elem := range command {
 		commandForOverride := fmt.Sprintf(override + " -b " + barName + " -m " + elem + " -k " + appName)
+		fmt.Println(commandForOverride)
 		cmd := exec.Command("bash", "-c", commandForOverride)
 		err := cmd.Run()
 		fmt.Println(err)
 	}
-
-	//fmt.Println(commandForOverride)
-
 }
